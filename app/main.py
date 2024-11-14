3ddc23eb448b80c6cf95b0229e218e9e4ab49222
tzevi stern <z02072000@gmail.com>
2024-11-14 10:29:47 +0200

    Add kafka settings for handling basic definitions of kafka

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from flask import request, jsonify, Flask
-
+from app.db.mongo_database import init_to_mongo_db
 app = Flask(__name__)
 
 
-@app.route('/new_member', methods=['POST'])
-def new_member():
-    print(request.json)
+@app.route('/api/email', methods=['POST'])
+def all_messages():
+    try:
+        data = request.json
+        if not data:
+            return jsonify({"error": "No data provided"}), 400
+        init_to_mongo_db(data)
+        return jsonify({"message": "Data inserted successfully"}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
